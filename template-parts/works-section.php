95538d9c55a541f6bb540192a8999431978559e7
horacegoal <horacegoal@gmail.com>
2023-06-30 18:54:58 +0800

    feat: work url

--- template-parts/works-section.php
+++ template-parts/works-section.php
@@ -19,35 +19,40 @@
             $title = get_the_title();
             // get the post featured image
             $featured_image = get_the_post_thumbnail_url();
-            // display title and image
+            $work_url = get_field('url');
         ?>
             <div class="work-container p-8">
-                <div class="bg-main p-4 h-full flex flex-col justify-between">
-                    <div>
-                        <div class="text-center font-semibold mb-2"><?php echo $title ?></div>
+                <?php if ($work_url) { ?>
+                    <a href="<?php echo $work_url ?>" target="_blank">
+                    <?php } ?>
+                    <div class="bg-main p-4 h-full flex flex-col justify-between">
+                        <div>
+                            <div class="text-center font-semibold mb-2"><?php echo $title ?></div>
 
-                        <div class="w-full work-image overflow-hidden">
-                            <img src=<?php echo $featured_image ?> alt="app cover" class="h-full">
+                            <div class="w-full work-image overflow-hidden">
+                                <img src=<?php echo $featured_image ?> alt="app cover" class="h-full">
 
+                            </div>
+                        </div>
+
+                        <div class="mt-4">
+                            <!-- display tags -->
+                            <?php
+                            $tags = get_the_tags();
+                            if ($tags) {
+                                foreach ($tags as $tag) {
+                                    // display tag inside div
+                            ?>
+                                    <div class="tag-container border px-2 py-1 inline-block text-xs"><?php echo $tag->name ?></div>
+                            <?php
+                                }
+                            }
+                            ?>
                         </div>
                     </div>
-
-                    <div class="mt-4">
-                        <!-- display tags -->
-                        <?php
-                        $tags = get_the_tags();
-                        if ($tags) {
-                            foreach ($tags as $tag) {
-                                // display tag inside div
-                        ?>
-                                <div class="tag-container border px-2 py-1 inline-block text-xs"><?php echo $tag->name ?></div>
-                        <?php
-                            }
-                        }
-                        ?>
-                    </div>
-                </div>
-
+                    <?php if ($work_url) { ?>
+                    </a>
+                <?php } ?>
             </div>
         <?php
         }
